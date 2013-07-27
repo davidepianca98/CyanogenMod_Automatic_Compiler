6f5bbc97438556b2f7612af1e39f3c9e7cc58cb2
KINGbabasula <kingbabasula@gmail.com>
2013-07-27 09:23:16 +0200

    Add FTP support and Number of builds

--- cm.sh
+++ cm.sh
@@ -2,8 +2,8 @@
 # CyanogenMod automatic compiler
 # by KINGbabasula
 #
+upp=1
 clear
-TIMES=1000
 echo
 echo Welcome to the KINGbabasula automatic CyanogenMod compiler
 echo
@@ -15,6 +15,40 @@ read delay
 echo
 echo -e "Please enter the number of jobs to compile and sync: \c "
 read job
+echo
+echo -e "Please enter how many builds the script has to do: \c"
+read TIMES
+echo
+echo -e "Do you want to upload the builds automatically to a ftp server?[1=yes|2=no] \c"
+read up
+if [ "$up" == "$upp" ]
+then
+    echo -e "Enter your ftp host: \c"
+    read host
+    echo -e "Enter your password: \c"
+    read pass
+    echo -e "Enter your username: \c"
+    read user
+    for a in $TIMES
+    do
+       . build/envsetup.sh
+       echo Lunching CyanogenMod for your device..
+       lunch cm_$codename-userdebug
+       echo Making clean...
+       make clean
+       echo Compiling...
+       make -j$job bacon
+       date=`date +%Y%m%d`
+       cd out/target/product/$codename
+       ftp -inv $HOST
+       user $USER $PASS
+       cd public_html/i9105P/cm-10.1
+       put cm-10.1-$date-UNOFFICIAL-$codename.zip
+       bye
+       sleep $delay
+       repo sync -j$job
+    done
+else
 for a in $TIMES
 do
    . build/envsetup.sh
@@ -27,3 +61,4 @@ do
    sleep $delay
    repo sync -j$job
 done
+fi
